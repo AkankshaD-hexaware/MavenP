0d4b7c4645a4233919354bd1c88a60034d5765fa
Akanksha Deotare <AkankshaD@hexaware.com>
2018-01-24 12:06:14 +0530

    first commit

---
+++ src/main/java/com/akanksha/Multiplication.java
@@ -0,0 +1,13 @@
+class Multiplication{
+
+    public int mul(int a,int b){
+
+        return(a*b);
+    }
+}
+class Multiplicationmain{
+    public static void main(String[] args){
+    Multiplication m=new Multiplication();
+    int result=m.mul(1,2);
+}
+}
